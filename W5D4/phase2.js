613206ca0242d17d3ff88b17475087f9ffaba8e5
Chu Jang <david.c.jang@gmail.com>
2017-09-01 09:44:45 -0700

    W5D5

--- W5D4/phase2.js
+++ W5D4/phase2.js
@@ -1,3 +1,10 @@
-var titleize = function(arr, callback) {
+// .load phase2.js
 
+var titleize = function(arr, callback) {
+  var capitalized = arr.map(name=>`Mx. ${name} Jingleheimer Schmidt`);
+  callback(capitalized);
+};
+
+var callback = function(names) {
+  return names.forEach(name=>console.log(name));
 };
